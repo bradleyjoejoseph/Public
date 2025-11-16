f88d06f843bd9cdf07161ef2edcd0a125d40ad4b
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-16 18:52:52 +0000

    uoy seminar week 8

---
+++ UOY/Seminar/week8.py
@@ -0,0 +1,14 @@
+def sum_all(nums):
+    total = 0
+
+    for i in range(len(nums)):
+        temp = nums[i]
+        if type(temp) == list:
+            # print(nums)
+            temp = sum_all(temp)
+        total += temp
+
+    return total
+
+# print(sum_all([1,[2,[3,[4]]]]))
+# answer is 10 program outputs 10
