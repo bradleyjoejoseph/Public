d4427abd103c74fe4690458f6e2e440796f284cb
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-17 11:37:33 +0000

    Update week8.py

---
+++ UOY/Lab/week8.py
@@ -0,0 +1,10 @@
+import math
+
+class Circle:
+    def __init__(self, centre, radius):
+        self.centre = centre
+        self.radius = radius
+    
+    def getArea():
+        return math.pi * self.radius**2
+    def getPerimeter()
