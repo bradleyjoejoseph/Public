795fadf45a967e5178c7acb228be721662b67503
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-18 16:31:28 +0000

    Update week8.py

--- UOY/Lab/week8.py
+++ UOY/Lab/week8.py
@@ -5,6 +5,30 @@ class Circle:
         self.centre = centre
         self.radius = radius
     
-    def getArea():
+    def getArea(self):
         return math.pi * self.radius**2
-    def getPerimeter()
+    def getPerimeter(self):
+        return math.pi * self.radius * 2
+
+class Vector:
+    def __init__(self, data = None):
+        self.data = data
+    def __str__(self):
+        if self.data == None:
+            return "<>"
+        self.data = [str(i) for i in self.data]
+        return '<' + ", ".join(self.data) + '>'
+    def dim(self):
+        return len(self.data)
+    def get(self, index):
+        return self.data[index]
+    def set(self, index, value):
+        self.data[index] = value
+    
+    def scalar_product(self, scalar):
+        return Vector()
+
+v = Vector([1, 2, 3])
+empV = Vector()
+
+print(v, empV, v.dim())
