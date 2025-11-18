9e6e3fbb295cea84f6f3b4a9bbff6711807092a4
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-18 17:37:15 +0000

    had to do "proper recursion"

--- UOY/Seminar/week8.py
+++ UOY/Seminar/week8.py
@@ -12,4 +12,16 @@ def sum_all(nums):
 
 # print(sum_all([1,[2,[3,[4]]]]))
 # answer is 10 program outputs 10
+# got told it wasnt proper recursion
 
+def sum_alll(nums):
+    if len(nums) == 0:
+        return 0
+    if type(nums[0]) == int:
+        return nums[0] + sum_alll(nums[1:])
+    else:
+        return sum_alll(nums[0]) + sum_alll(nums[1:])
+    
+
+print(sum_alll([1,[2,[3,[4]]]]))
+# made another solution same stuff but proper recursion
