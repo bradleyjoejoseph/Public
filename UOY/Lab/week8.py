54448fe290bd41ba8359cd00d9d4cf239644ff58
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-20 17:27:06 +0000

    remove test

--- UOY/Lab/week8.py
+++ UOY/Lab/week8.py
@@ -36,15 +36,15 @@ class Vector:
             return False
         return True
 
-v = Vector([1, 2, 3])
-empV = Vector()
+# v = Vector([1, 2, 3])
+# empV = Vector()
 
-print(v) # should print <1, 2, 3>
-print(empV) # should print <>
-print(v.dim()) # should print 3
-print(v.get(1)) # should print 2
-v.set(2, 4)
-print(v.get(2)) # should print 4
-print(v.scalar_product([3, 2, 3])) # should print <3, 4, 12>
-print(v.add([2, 3, 4])) # should print <3, 5, 8>
-print(v.equals([1, 2, 4]), v.equals([1, 2, 3])) # should print True False
+# print(v) # should print <1, 2, 3>
+# print(empV) # should print <>
+# print(v.dim()) # should print 3
+# print(v.get(1)) # should print 2
+# v.set(2, 4)
+# print(v.get(2)) # should print 4
+# print(v.scalar_product([3, 2, 3])) # should print <3, 4, 12>
+# print(v.add([2, 3, 4])) # should print <3, 5, 8>
+# print(v.equals([1, 2, 4]), v.equals([1, 2, 3])) # should print True False
