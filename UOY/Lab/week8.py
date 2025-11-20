34fadb2602367e3db46ef611cee08f903ddceaa7
Bradley Joseph <bradleyjoe007@gmail.com>
2025-11-20 17:18:45 +0000

    scalar product

--- UOY/Lab/week8.py
+++ UOY/Lab/week8.py
@@ -26,9 +26,15 @@ class Vector:
         self.data[index] = value
     
     def scalar_product(self, scalar):
-        return Vector()
+        return Vector([int(x) * y for x, y in zip(self.data, scalar)])
 
 v = Vector([1, 2, 3])
 empV = Vector()
 
-print(v, empV, v.dim())
+print(v) # should print <1, 2, 3>
+print(empV) # should print <>
+print(v.dim()) # should print 3
+print(v.get(1)) # should print 2
+v.set(2, 4)
+print(v.get(2)) # should print 4
+print(v.scalar_product([3, 2, 3])) # should print <3, 4, 12>
