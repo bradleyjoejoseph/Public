e8717f2c1a1e97ffa5ec229cd200d5101efb0ddf
Bradley Joseph <bradleyjoe007@gmail.com>
2026-02-17 15:53:26 +0000

    Add TextUtils.toBase10 and use in App
    Introduce sof2week02softwarelab.TextUtils with a toBase10(String) method that converts a binary string to its decimal value (iterates over chars and sums powers of two). Update App.main to call TextUtils.toBase10("10001011") and print the result. Compiled class files for App and TextUtils are also added to bin.

--- UOY/Lab/SO2/week2/src/App.java
+++ UOY/Lab/SO2/week2/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(sof2week02softwarelab.TextUtils.toBase10("10001011"));
     }
 }
---
+++ UOY/Lab/SO2/week2/src/sof2week02softwarelab/TextUtils.java
@@ -0,0 +1,17 @@
+package sof2week02softwarelab;
+
+public class TextUtils {
+
+    public static int toBase10(String binary){
+        int result = 0;
+        int lenny = binary.length()-1;
+        for (int i = lenny; i >= 0; i--) {
+            if (binary.charAt(i) == '1') {
+                result += Math.pow(2, lenny-i);
+            }
+        }
+
+
+        return result;
+    }
+}
